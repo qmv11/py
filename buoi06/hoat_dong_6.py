8867ba1781d6a4f8a8bff03753d48bb37eb66c9e
qmv11 <quocmanh1011@gmail.com>
2026-09-25 09:26:08 +0700

    Hoan thanh buoi06

---
+++ buoi06/hoat_dong_6.py
@@ -0,0 +1,22 @@
+#6.1
+print("Bai 6.1:\n")
+def giai_thua_de_quy(n):
+    if n <= 1: # dieu kien dung
+        return 1
+    return n * giai_thua_de_quy(n - 1)
+def giai_thua_lap(n):
+    ket_qua = 1
+    for i in range(1, n + 1):
+        ket_qua *= i
+    return ket_qua
+print(giai_thua_de_quy(5), "-", giai_thua_lap(5))
+
+#6.2
+print("\nBai 6.2:\n")
+def fibonacci_de_quy(n):
+    if n <= 1: # dieu kien dung
+        return n
+    return fibonacci_de_quy(n - 1) + fibonacci_de_quy(n - 2)
+for i in range(10):
+    print(fibonacci_de_quy(i), end=" ")
+print()
